169904ca6d158b44eb90e240528327c8ebaa236a
Jiaqi Li <ljq0831@qq.com>
2024-12-15 13:15:20 +0800

    Update: "_posts/2024-11-26-bazel\347\274\226\350\257\221go\351\241\271\347\233\256.md","_posts/2024-12-15-swiftui\345\255\246\344\271\240\347\254\224\350\256\260.md",scripts/touch_new_post.sh
    Signed-off-by: Jiaqi Li <ljq0831@qq.com>

--- scripts/touch_new_post.sh
+++ scripts/touch_new_post.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
 # 读取日期
-read -p "Enter the date (leave bland for now): " current_date
+read -p "Enter the date (leave blank for now): " date_input
 # 读取标题
 read -p "Enter the post title: " title
 # 读取摘录
@@ -10,43 +9,43 @@ read -p "Enter the excerpt (leave blank for none): " excerpt
 # 读取分类
 read -p "Enter the categories (comma separated, leave blank for 'work'): " categories_input
 # 读取标签
-read -p "Enter the tags (comma separated, leave blank for default 'WIP'): " tags_input
+read -p "Enter the tags (comma separated, leave blank for none): " tags_input
 
 # 处理日期
 if [ -z "$date_input" ]; then
-    # 获取当前日期
-    current_date=$(date +"%Y-%m-%d")
+    date_input=$(date +"%Y-%m-%d")
 fi
 
 # 处理分类
 if [ -z "$categories_input" ]; then
     categories="work"
+else
+    IFS=',' read -r -a categories_array <<< "$categories_input"
+    categories=$(IFS=','; echo "${categories_array[*]}")
 fi
 
 # 处理标签
-if [ -z "$tags_input" ]; then
-    tags="  - WIP"
-else
+tags=""
+if [ -n "$tags_input" ]; then
     IFS=',' read -r -a tags_array <<< "$tags_input"
     tags=$(printf "  - %s\n" "${tags_array[@]}")
 fi
 
 # 构建文件名
-filename="${current_date}-$(echo $title | tr ' ' '-' | tr '[:upper:]' '[:lower:]').md"
+filename="${date_input}-$(echo $title | tr ' ' '-' | tr '[:upper:]' '[:lower:]').md"
 
 # 文件路径
 posts_dir="_posts"
-mkdir -p "$posts_dir"  # 确保_post文件夹存在
+mkdir -p "$posts_dir"
 file_path="$posts_dir/$filename"
 
 # 创建文件内容
 {
     echo "---"
     echo "title: $title"
-    echo "excerpt: $excerpt"
+    [ -n "$excerpt" ] && echo "excerpt: $excerpt"
     echo "categories: $categories"
-    echo "tags:"
-    echo "$tags"
+    [ -n "$tags" ] && echo "tags:" && echo "$tags"
     echo "---"
 } > "$file_path"
 
